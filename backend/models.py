36c5b5837beaff61e4c36924c210ea2fee104c90
SamVyk <samnangvyvath.khun@student.uts.edu.au>
2026-04-07 14:52:56 +1000

    update 18.1#

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import  column, Integer, Text, TIMESTAMP, text
+from sqlalchemy import  Column, Integer, Text, TIMESTAMP, text
 from database import Base
 class Flashcard(Base):
     __tablename__ = "flashcards"
-    id = column(Integer, primary_key=True, index=True)
-    question = column(Text, nullable=False)
-    answer = column(Text, nullable=False)
-    user_id = column(TIMESTAMP, server_default=text("CURRENT_TIMESTAMP"))
+    id = Column(Integer, primary_key=True, index=True)
+    question = Column(Text, nullable=False)
+    answer = Column(Text, nullable=False)
+    user_id = Column(TIMESTAMP, server_default=text("CURRENT_TIMESTAMP"))
